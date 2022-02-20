f793084ed525730448acb3463fbab1e0a966a1f0
konmin <MindlinKS@yandex.ru>
2022-02-20 12:00:46 +0300

    fix after code review

--- main.py
+++ main.py
@@ -14,50 +14,49 @@ NEW_TASK_COMMAND_NAME = 'new_task'
 LIST_TASK_COMMAND_NAME = 'list_task'
 
 
+def command(func):
+    def wrapper(update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+        try:
+            init_user(chat_id)
+            func(update, context, chat_id)
+        except Exception as e:
+            print(f"ERROR: {e}")
+            context.bot.send_message(chat_id, text="Oops")
+    return wrapper
+
+
 def init_user(chat_id):
     if chat_id not in users_task_data:
         users_task_data[chat_id] = []
 
 
-def start(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    try:
-        init_user(chat_id)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    except Exception as e:
-        print(e)
-        context.bot.send_message(chat_id, text="Oops")
+@command
+def start(update: Update, context: CallbackContext, chat_id: int) -> None:
+    init_user(chat_id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-def new_task(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    try:
-        user_message = update.message.text[len(NEW_TASK_COMMAND_NAME) + 2:]
-        init_user(chat_id)
-        if user_message:
-            users_task_data[chat_id].append(user_message)
-            print(users_task_data)
-            context.bot.send_message(chat_id, text="Ok")
-        else:
-            print(users_task_data)
-            context.bot.send_message(chat_id, text="Введите название задачи")
-    except Exception as e:
-        print(e)
-        context.bot.send_message(chat_id, text="Oops")
-
-
-def list_task(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    try:
-        init_user(chat_id)
+@command
+def new_task(update: Update, context: CallbackContext, chat_id: int) -> None:
+    init_user(chat_id)
+    user_message = " ".join(context.args)
+    if user_message:
+        users_task_data[chat_id].append(user_message)
         print(users_task_data)
-        task_text = '\n'.join([f"{index + 1}. {value}" for index, value in enumerate(users_task_data[chat_id])])
-        if not task_text:
-            task_text = "Нет задач. Создайте хотя бы одну задачу"
-        context.bot.send_message(chat_id, text=task_text)
-    except Exception as e:
-        print(e)
-        context.bot.send_message(chat_id, text="Oops")
+        context.bot.send_message(chat_id, text="Ok")
+    else:
+        print(users_task_data)
+        context.bot.send_message(chat_id, text="Введите название задачи")
+
+
+@command
+def list_task(update: Update, context: CallbackContext, chat_id: int) -> None:
+    print(users_task_data)
+    task_text = '\n'.join([f"{index + 1}. {value}" for index, value in enumerate(users_task_data[chat_id])])
+    if not task_text:
+        task_text = "Нет задач. Создайте хотя бы одну задачу"
+    context.bot.send_message(chat_id, text=task_text)
 
 
 if __name__ == "__main__":
