b4537c2b1c073c53db0ab342f6f606eaea51fb14
konmin <MindlinKS@yandex.ru>
2022-02-24 10:01:47 +0300

    add del_task

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ access_token = os.getenv('ACCESS_TOKEN')
 users_task_data: dict[Union[int, str], list[str]] = {}
 NEW_TASK_COMMAND_NAME = 'new_task'
 LIST_TASK_COMMAND_NAME = 'list_task'
+DEL_TASK_COMMAND_NAME = 'del_task'
 
 
 def command(func):
@@ -33,13 +34,11 @@ def init_user(chat_id):
 
 @command
 def start(update: Update, context: CallbackContext, chat_id: int) -> None:
-    init_user(chat_id)
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
 @command
 def new_task(update: Update, context: CallbackContext, chat_id: int) -> None:
-    init_user(chat_id)
     user_message = " ".join(context.args)
     if user_message:
         users_task_data[chat_id].append(user_message)
@@ -59,6 +58,28 @@ def list_task(update: Update, context: CallbackContext, chat_id: int) -> None:
     context.bot.send_message(chat_id, text=task_text)
 
 
+@command
+def del_task(update: Update, context: CallbackContext, chat_id: int) -> None:
+    if not users_task_data[chat_id]:
+        context.bot.send_message(chat_id, text="У вас нет задач для удаления")
+        return
+
+    try:
+        task_index = int(context.args[0]) - 1
+    except ValueError:
+        context.bot.send_message(chat_id, text="Значением индекса должно быть число")
+        return
+
+    max_task_index = len(users_task_data[chat_id])
+    if task_index >= max_task_index or task_index < 0:
+        context.bot.send_message(chat_id,
+                                 text=f"Индекс задачи не находится в допустимом диапазоне [1:{max_task_index}]")
+        return
+
+    del users_task_data[chat_id][task_index]
+    context.bot.send_message(chat_id, text="Задача удалена")
+
+
 if __name__ == "__main__":
     updater = Updater(token=access_token, use_context=True)
     dispatcher = updater.dispatcher
@@ -71,5 +92,8 @@ if __name__ == "__main__":
 
     list_task_handler = CommandHandler(LIST_TASK_COMMAND_NAME, list_task)
     dispatcher.add_handler(list_task_handler)
+
+    del_task_handler = CommandHandler(DEL_TASK_COMMAND_NAME, del_task)
+    dispatcher.add_handler(del_task_handler)
 
     updater.start_polling()
